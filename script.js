9906ab8cc7cbce32a9cb9d18e70560d47ed123db
marz <marseceva@gmail.com>
2026-08-30 15:54:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -766,7 +766,7 @@ const previewMessages = [
         "DOVE! RAAAAAAH RAAAAAAH RAAAAAAH",
         "#DAA520",
         "195845559",
-        { badges: "vip/1,pikachu/1" }
+        { badges: "mod/1,subscriber/1,pikachu/1" }
     ]
 ];
 
