75b427c2730ff85b2d30b88c54f3d4dcee2c7ea3
marz <marseceva@gmail.com>
2026-08-30 16:05:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -360,6 +360,20 @@ let backgroundColor =
         ? overlaySettings.backgroundColor
         : "#2d0c12";
 
+function hexToRgbaString(hex, alpha) {
+    let h = hex.replace("#", "");
+
+    if (h.length === 3) {
+        h = h.split("").map(c => c + c).join("");
+    }
+
+    const r = parseInt(h.slice(0, 2), 16);
+    const g = parseInt(h.slice(2, 4), 16);
+    const b = parseInt(h.slice(4, 6), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
 function applyBackgroundColor(hex) {
     document.documentElement.style.setProperty(
         "--bg-color-1",
